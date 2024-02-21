1e2cf2ba963534626820aef27cfdc343ccd6d5df
Vignesh Mohan <vignesh.mohan@colanonline.com>
2024-02-21 19:32:53 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-800">Welcome to the world</h1>
+      <p className="">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        molestias sed harum? Est atque vero recusandae sunt vel accusamus
+        excepturi impedit, saepe amet! Laudantium totam illum delectus rem vel.
+        Consectetur?
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Home from './components/Home/Home.jsx'
+import Layout from './Layout.jsx'
+import About from './components/About/About.jsx'
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about/' element={<About/>}/>
+    </Route>
+  )
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
